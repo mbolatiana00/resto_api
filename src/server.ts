74b01dd7819c7229360ba7e693e47cc80a2b0088
Mbola <lovambolatiana@gmail.com>
2026-03-04 14:03:49 +0100

    correction server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app";
-console.log(app);
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
